cd2712fb6c51f477f69c95a4913b652fc6d59b31
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2026-06-24 18:28:29 +0200

    feat: add "screenshots" to webmanifest

--- src/webmanifest/webmanifest.ts
+++ src/webmanifest/webmanifest.ts
@@ -3,6 +3,11 @@ import type { WebAppManifest } from 'web-app-manifest';
 import icon192 from './icons/192.png';
 import icon512 from './icons/512.png';
 
+import screenDesktopEdit from './screenshots/edit-screen_desktop_2026-06-24.png';
+import screenMobileEdit from './screenshots/edit-screen_mobile_2026-06-24.png';
+import screenDesktopList from './screenshots/list-screen_desktop_2026-06-24.png';
+import screenMobileList from './screenshots/list-screen_mobile_2026-06-24.png';
+
 export default {
 	name: 'MeCards',
 	background_color: '#1C9AF3',
@@ -25,6 +30,32 @@ export default {
 			src: icon512,
 			type: 'image/png',
 		},
+	],
+	screenshots: [
+		{
+      form_factor: 'narrow',
+      sizes: '1290x2796',
+      src: screenMobileEdit,
+      type: 'image/png',
+    },
+		{
+      form_factor: 'narrow',
+      sizes: '1290x2796',
+      src: screenMobileList,
+      type: 'image/png',
+    },
+		{
+      form_factor: 'wide',
+      sizes: '2880x1580',
+      src: screenDesktopEdit,
+      type: 'image/png',
+    },
+		{
+      form_factor: 'wide',
+      sizes: '2880x1580',
+      src: screenDesktopList,
+      type: 'image/png',
+    },
 	],
 	start_url: '/',
 	prefer_related_applications: false
