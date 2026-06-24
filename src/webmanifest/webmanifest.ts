89e6062112449bdbebe459bc60bd8d18411e9792
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2026-06-24 18:10:56 +0200

    fix: webmanifest icons `type` mime (`impage/png` → `image/png`)

--- src/webmanifest/webmanifest.ts
+++ src/webmanifest/webmanifest.ts
@@ -4,28 +4,28 @@ import icon192 from './icons/192.png';
 import icon512 from './icons/512.png';
 
 export default {
-	name: "MeCards",
-	background_color: "#1C9AF3",
-	theme_color: "#1C9AF3",
-	display: "standalone",
+	name: 'MeCards',
+	background_color: '#1C9AF3',
+	theme_color: '#1C9AF3',
+	display: 'standalone',
 	display_override: [
-		"standalone",
-		"minimal-ui",
+		'standalone',
+		'minimal-ui',
 	],
 	icons: [
 		{
 			purpose: 'any',
 			sizes: '192x192',
 			src: icon192,
-			type: 'impage/png',
+			type: 'image/png',
 		},
 		{
 			purpose: 'any',
 			sizes: '512x512',
 			src: icon512,
-			type: 'impage/png',
+			type: 'image/png',
 		},
 	],
-	start_url: "/",
+	start_url: '/',
 	prefer_related_applications: false
 } satisfies WebAppManifest;
