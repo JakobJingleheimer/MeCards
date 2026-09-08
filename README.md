f4a5655ee9508c74185da85aee0ab63e9ff1cfb8
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2026-09-08 14:55:09 +0200

    doc: update README

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-Generate uid with nanoid (6 char)
+Inspired by StoCard; RIP.
 
-Save SVGs in CacheStorage, filename `${uid}.svg`
-Save Card details in LocalStorage, keyed by uid
+This app is not monetised in any way, and there is no intention to ever do so.
+
+It’s open-source, and works fully offline (except when fetching a merchant’s logo from [WikiMedia](https://commons.wikimedia.org/)). All data you enter is stored locally on your device and never sent to third parties. The app is solely a client application—it has no server and nowhere to send any of your information.
+
+There’s currently no analytics or telemetry; if added in future, it will be a privacy-centric feature (for instance, automatically creating anoynomised bug reports in the project repo when an error is encountered).
--- src/app/About.tsx
+++ src/app/About.tsx
@@ -108,7 +108,7 @@ export default function About() {
 
 			<p>This app is not monetised in any way, and there is no intention to ever do so.</p>
 
-			<p>It’s (<a href={pjson.homepage}>open-source</a>), and works fully offline (except when fetching a merchant’s logo from <a href="https://commons.wikimedia.org/">WikiMedia</a>). All data you enter is stored locally on your device and never sent to third parties. The app is solely a client application—it has no server and nowhere to send any of your information.</p>
+			<p>It’s <a href={pjson.homepage}>open-source</a>, and works fully offline (except when fetching a merchant’s logo from <a href="https://commons.wikimedia.org/">WikiMedia</a>). All data you enter is stored locally on your device and never sent to third parties. The app is solely a client application—it has no server and nowhere to send any of your information.</p>
 
 			<p>There’s currently no analytics or telemetry; if added in future, it will be a privacy-centric feature (for instance, automatically creating anoynomised bug reports in the project repo when an error is encountered).</p>
 		</main>
