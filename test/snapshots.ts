dc02867a877600b98295756f99ea09f6ec015aa9
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2026-09-14 22:11:25 +0200

    test: add snapshot filename fixer

---
+++ test/snapshots.ts
@@ -0,0 +1,14 @@
+import { basename, dirname, extname, join } from 'node:path';
+import { snapshot } from 'node:test';
+
+snapshot.setResolveSnapshotPath(generateSnapshotPath);
+
+function generateSnapshotPath(testFilePath) {
+	if (!testFilePath) return '';
+
+	const ext = extname(testFilePath);
+	const filename = basename(testFilePath, ext);
+	const base = dirname(testFilePath);
+
+	return join(base, `${filename}.snap.cjs`);
+}
